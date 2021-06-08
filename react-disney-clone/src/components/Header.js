5c18a7e1fc7cc1ee2dd968b051d9de60bd00881d
Tatien Miller <tatmil99@gmail.com>
2021-06-08 15:25:39 -0500

    adds firebase to project

--- react-disney-clone/src/components/Header.js
+++ react-disney-clone/src/components/Header.js
@@ -106,9 +106,9 @@ const NavMenu = styled.div`
   }
 
 
-  /* @media (max-width: 768px) {
+  @media (max-width: 768px) {
     display: none;
-  } */
+  }
 `;
 
 const Login = styled.a`
---
+++ react-disney-clone/src/firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Y39pt6mkmcrhgU3KO29lbVs6Tfit8zw",
+  authDomain: "disneyplus-clone-b2b38.firebaseapp.com",
+  projectId: "disneyplus-clone-b2b38",
+  storageBucket: "disneyplus-clone-b2b38.appspot.com",
+  messagingSenderId: "631291706299",
+  appId: "1:631291706299:web:4343a5e60760025c56e29e",
+  measurementId: "G-ZWWR4E1099"
+};
+
+const firebaseApp = firebase.initialize(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
