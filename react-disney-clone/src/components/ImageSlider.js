3dc08182631bd36783b3bd4cab8c271a152b749e
Tatien Miller <tatmil99@gmail.com>
2021-06-09 15:42:48 -0500

    adds slider carousel and is functional

--- react-disney-clone/src/components/ImageSlider.js
+++ react-disney-clone/src/components/ImageSlider.js
@@ -15,15 +15,29 @@ const ImageSlider = () => {
   }
   return (
     <Carousel {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
+      <Wrap>
+        <a>
+          <img src="/images/slider-badging.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src="/images/slider-scale.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src="/images/slider-badag.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a>
+          <img src="/images/slider-scales.jpg" alt="" />
+        </a>
+      </Wrap>
     </Carousel>
   );
 }
@@ -39,6 +53,54 @@ const Carousel = styled(Slider)`
 
     &:hover {
       opacity: 1;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    border-radius: 4px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+    box-shadow: 0px 26px 30px -5px rgb(0 0 0 / 80%);
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover {
+    padding: 0;
+    border: solid 4px rgba(249, 249, 249, 0.8);
     }
   }
 `;
