60ad933a3eaebc7d531c129a53d52ad281d8e016
Tatien Miller <tatmil99@gmail.com>
2021-06-09 14:18:52 -0500

    adds react slick and carousel to dependencies

--- react-disney-clone/src/components/Home.js
+++ react-disney-clone/src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from "styled-components";
+import ImageSlider from './ImageSlider';
 
 const Home = () => {
   return (
     <Container>
-
+      <ImageSlider />
     </Container>
   );
 }
---
+++ react-disney-clone/src/components/ImageSlider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components";
+
+const ImageSlider = () => {
+  return (
+    <div>
+      Image Slider
+    </div>
+  )
+}
+
+export default ImageSlider
